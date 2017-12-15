4fd80486f42a073d879ca888e69c1dadd7a16e9d
Rayan Mahanna <rayan.mahanna@devinci.fr>
2017-12-15 17:10:28 +0100

    Exercice 4

--- src/Str.php
+++ src/Str.php
@@ -101,4 +101,23 @@ class Str
         }
     }
 
+    //Exercice 4
+
+    public function slugCase()
+    {
+        if (preg_match("/[-,_, ]+/", $this->string) === 1){
+            return $this
+                ->replace('_', ' ')
+                ->ucwords()
+                ->replace(' ', '-')
+                ->strtolower();
+        }
+        else{
+            $patern ="/(.)(?=[A-Z])/";
+            $this->string = preg_replace($patern, '$1-',$this->string);
+            return $this
+                ->strtolower();
+        }
+    }
+
 }
